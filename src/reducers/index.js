d50ad41ef06b29e785575f33c9ffad8f2def6008
lightsage88 <adrian.e.rosales@gmail.com>
2018-01-19 03:21:42 -0800

    first commit set up a few early things

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+
+
+const initialState = {
+auralStatus: '',
+correctAnswer: Math.round(Math.random() * 100) + 1,
+feedback: "Make your guess!",
+guesses: []
+
+};
+
+export const gameReducer = (state=initialState, action) => {
+
+};
+
